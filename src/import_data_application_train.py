889549f03e46ef174c4c0352411cd0817cad73a1
Raditya Zaki Athaya <18223086@std.stei.itb.ac.id>
2026-06-07 12:16:28 +0700

    feat: add python code for import data

--- src/import_data_application_train.py
+++ src/import_data_application_train.py
@@ -6,8 +6,8 @@ import time
 db_uri = 'postgresql+psycopg2://postgres:YOUR_PASSWORD@localhost:5433/home_credit_risk'
 engine = create_engine(db_uri)
 
-# 2. Target file kedua
-file_path = 'data/bureau.csv'
+# 2. Target file ketiga
+file_path = 'data/application_train.csv'
 
 print("Membaca file CSV ke dalam Pandas... (Tunggu sebentar)")
 start_time = time.time()
@@ -22,8 +22,8 @@ print("Mengirim data ke PostgreSQL dengan teknik chunking...")
 # 3. Eksekusi pengiriman bertahap
 with engine.begin() as conn:
     # chunksize=50000 berarti mengirim 50.000 baris per transaksi
-    df.to_sql('bureau', con=conn, if_exists='replace', index=False, chunksize=50000)
+    df.to_sql('application_train', con=conn, if_exists='replace', index=False, chunksize=50000)
 
 end_time = time.time()
-print(f"Berhasil! Tabel 'bureau' sudah masuk.")
+print(f"Berhasil! Tabel 'application_train' sudah masuk.")
 print(f"Total waktu eksekusi: {round(end_time - start_time, 2)} detik.")
